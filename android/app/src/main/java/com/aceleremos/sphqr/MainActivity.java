b0a8e06b0e14d14528e19cfa614b99ba169b7d09
Jereff <jereff@aceleremos.com>
2026-01-16 02:56:32 -0600

    feat(android): integra Capacitor y proyecto nativo para Android

---
+++ android/app/src/main/java/com/aceleremos/sphqr/MainActivity.java
@@ -0,0 +1,5 @@
+package com.aceleremos.sphqr;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.aceleremos.sphqr',
+  appName: 'SPH Control de Accesos',
+  webDir: 'dist'
+};
+
+export default config;
